2a18fc76ee33d4ba6297e5e729fc426d990dc372
elisaliuliu <eliisaliiu8@gmail.com>
2022-03-29 11:58:38 -0700

    i

--- src/ca.ubc.cpsc304/controller/Hotel.java
+++
@@ -1,8 +0,0 @@
-package ca.ubc.cs304.controller;
-
-import ca.ubc.cs304.database.DatabaseConnectionHandler;
-import ca.ubc.cs304.delegates.LoginWindowDelegate;
-import ca.ubc.cs304.delegates.TerminalTransactionsDelegate;
-import ca.ubc.cs304.model.BranchModel ;
-import ca.ubc.cs304.ui.LoginWindow;
-import ca.ubc.cs304.ui.TerminalTransactions;
